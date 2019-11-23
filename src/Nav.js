21bfa644910b77a05f3131053f0dd3f988441042
Heimot <heimonenjoonas@gmail.com>
2019-11-23 16:05:52 +0200

    Added posting and added half of the patching thing

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, InputGroupAddon, Button, Input, CardText, CardBlock, Card, CardTitle } from 'reactstrap';
 import Dialog from './components/editDialog';
-import {Redirect} from 'react-router-dom';
-import "./Styles/NavStyles.css";
+import { Redirect } from 'react-router-dom';
+import "./Styles/Buttons.css";
 
 export default class TopNav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isOpen: false,
-      redirect: false
+      redirect: false,
+      kauppa: '',
+      nimi1: '',
+      maara1: '',
+      kerays1: '',
+
+      nimi2: '',
+      maara2: '',
+      kerays2: '',
+
+      nimi3: '',
+      maara3: '',
+      kerays3: '',
+
+      nimi4: '',
+      maara4: '',
+      kerays4: '',
+
+      nimi5: '',
+      maara5: '',
+      kerays5: '',
+
     };
     this.toggle = this.toggle.bind(this);
     this.logOut = this.logOut.bind(this);
@@ -23,14 +44,67 @@ export default class TopNav extends React.Component {
     });
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  postData() {
+    fetch('http://localhost:3002/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('userData')
+      },
+      body: JSON.stringify({
+        kauppa: this.state.kauppa,
+        kukka: {
+          kukka1: {
+            name: this.state.nimi1,
+            toimi: this.state.maara1,
+            kerays: this.state.kerays1,
+          },
+          kukka2: {
+            name: this.state.nimi2,
+            toimi: this.state.maara2,
+            kerays: this.state.kerays2,
+          },
+          kukka3: {
+            name: this.state.nimi3,
+            toimi: this.state.maara3,
+            kerays: this.state.kerays3,
+          },
+          kukka4: {
+            name: this.state.nimi4,
+            toimi: this.state.maara4,
+            kerays: this.state.kerays4,
+          },
+          kukka5: {
+            name: this.state.nimi5,
+            toimi: this.state.maara5,
+            kerays: this.state.kerays5,
+          },
+        }
+      }),
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
   render() {
-    if(this.state.redirect) {
-      return (<Redirect to={'/'}/>)
+    if (this.state.redirect) {
+      return (<Redirect to={'/'} />)
     }
     return (
       <div>
@@ -41,20 +115,161 @@ export default class TopNav extends React.Component {
             <Nav className="ml-auto" navbar>
               <Input placeholder="Kukan nimi" id="number" type="string" onChange={(evt) => { global.nimi11 = evt.target.value; }} />
               <InputGroupAddon>
-              <Button onClick={(e) => this.setState({ isOpen: true })}>Search
+                <Button>Search
               <Dialog isOpen={this.state.isOpen} onClose={(e) => this.setState({ isOpen: false })}>
-              <Card>
-                <CardBlock>
-                  <CardTitle><Input placeholder={'Kaupan nimi'}></Input></CardTitle>
-                  <CardText> <CardText className="label">Tuote</CardText><CardText className="label">tilattu määrä yht.</CardText><CardText className="label">toimitetaan</CardText><CardText className="label">lisätieto</CardText><CardText className="label">jäljellä</CardText><CardText className="label">keräyspiste</CardText></CardText>
-                  <CardText><Input className="inputlabel" placeholder={'Nimi'}></Input><Input className="inputlabel" placeholder={'Määrä'}></Input><Input className="inputlabel" placeholder={'Toimitetaan'}></Input><Input className="inputlabel" placeholder={'Lisätieto'}></Input><Input className="inputlabel" placeholder={'Jäljellä'}></Input><Input className="inputlabel" placeholder={'Keräys'}></Input></CardText>
-                  <CardText><Input className="inputlabel" placeholder={'Nimi'}></Input><Input className="inputlabel" placeholder={'Määrä'}></Input><Input className="inputlabel" placeholder={'Toimitetaan'}></Input><Input className="inputlabel" placeholder={'Lisätieto'}></Input><Input className="inputlabel" placeholder={'Jäljellä'}></Input><Input className="inputlabel" placeholder={'Keräys'}></Input></CardText>
-                </CardBlock>
-              </Card>
-              </Dialog>
-              </Button>
+                    <Card>
+                      <CardBlock>
+
+                        <CardTitle>
+
+                          <Input name="kauppa"
+                            onChange={this.handleChange}
+                            placeholder={'Kaupan nimi'}>
+                          </Input>
+
+                        </CardTitle>
+
+                        <CardText>
+
+                          <CardText className="label">
+                            Tuote
+                      </CardText>
+
+                          <CardText className="label">
+                            Kerätään
+                        </CardText>
+
+                          <CardText className="label">
+                            Keräyspiste
+                          </CardText>
+
+                        </CardText>
+
+                        <CardText>
+
+                          <Input className="inputlabel"
+                            name="nimi1"
+                            onChange={this.handleChange}
+                            placeholder={'Nimi'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="maara1"
+                            onChange={this.handleChange}
+                            placeholder={'Määrä'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="kerays1"
+                            onChange={this.handleChange}
+                            placeholder={'Keräyspiste'}>
+                          </Input>
+
+                        </CardText>
+
+                        <CardText>
+
+                          <Input className="inputlabel"
+                            name="nimi2"
+                            onChange={this.handleChange}
+                            placeholder={'Nimi'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="maara2"
+                            onChange={this.handleChange}
+                            placeholder={'Määrä'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="kerays2"
+                            onChange={this.handleChange}
+                            placeholder={'Keräyspiste'}>
+                          </Input>
+
+
+                        </CardText>
+
+                        <CardText>
+
+                          <Input className="inputlabel"
+                            name="nimi3"
+                            onChange={this.handleChange}
+                            placeholder={'Nimi'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="maara3"
+                            onChange={this.handleChange}
+                            placeholder={'Määrä'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="kerays3"
+                            onChange={this.handleChange}
+                            placeholder={'Keräyspiste'}>
+                          </Input>
+
+
+                        </CardText>
+
+                        <CardText>
+
+                          <Input className="inputlabel"
+                            name="nimi4"
+                            onChange={this.handleChange}
+                            placeholder={'Nimi'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="maara4"
+                            onChange={this.handleChange}
+                            placeholder={'Määrä'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="kerays4"
+                            onChange={this.handleChange}
+                            placeholder={'Keräyspiste'}>
+                          </Input>
+
+
+                        </CardText>
+
+                        <CardText>
+
+                          <Input className="inputlabel"
+                            name="nimi5"
+                            onChange={this.handleChange}
+                            placeholder={'Nimi'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="maara5"
+                            onChange={this.handleChange}
+                            placeholder={'Määrä'}>
+                          </Input>
+
+                          <Input className="inputlabel"
+                            name="kerays5"
+                            onChange={this.handleChange}
+                            placeholder={'Keräyspiste'}>
+                          </Input>
+
+
+                        </CardText>
+
+                        <Button onClick={() => this.postData()}>
+                          Luo uusi
+                          </Button>
+
+                      </CardBlock>
+                    </Card>
+                  </Dialog>
+                </Button>
               </InputGroupAddon>
-              <Button className='buttonCSS' type='button' onClick={() => this.logOut()}>Kirjaudu ulos</Button>
+              <button className='addBtn' type='button' onClick={(e) => this.setState({ isOpen: true })}></button>
+              <button type='button' onClick={() => this.logOut()}></button>
               <NavItem>
                 <NavLink href="/main">Components</NavLink>
               </NavItem>
--- src/PeopleCard.js
+++ src/PeopleCard.js
@@ -9,18 +9,49 @@ class PeopleCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      kukka1name: 'default',
-      kukka1toimi: 0,
-      username: "",
-      password: "",
+      nimi1: '',
+      maara1: '',
+      kerays1: '',
+
+      nimi2: '',
+      maara2: '',
+      kerays2: '',
+
+      nimi3: '',
+      maara3: '',
+      kerays3: '',
+
+      nimi4: '',
+      maara4: '',
+      kerays4: '',
+
+      nimi5: '',
+      maara5: '',
+      kerays5: '',
+
       isOpen: false,
       isOpen2: true
     }
-    this.login = this.login.bind(this);
   }
 
-  login() {
-    console.log(this.username + this.password);
+  patchData(_id) {
+    fetch('http://localhost:3002/products' + _id, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('userData')
+      },
+      body: JSON.stringify({
+        
+      }),
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   handleChange = (event) => {
@@ -48,30 +79,243 @@ class PeopleCard extends Component {
                   </CardText>
             </CardText>
 
-            {kukka.kukka1.toimi !== 0 ? 
-            <CardText>
-              <CardText className="label">
-                {kukka.kukka1.name}
+            {kukka.kukka1.toimi !== 0 ?
+              <CardText>
+                <CardText className="label">
+                  {kukka.kukka1.name}
                 </CardText>
                 <CardText className="label"
                 >{kukka.kukka1.toimi}
                 </CardText>
                 <CardText className="label">
                   {kukka.kukka1.kerays}
-                  </CardText>
-                  </CardText>
-                   : undefined}
+                </CardText>
+              </CardText>
+              : undefined}
+
+            {kukka.kukka2.toimi !== 0 ?
+              <CardText>
+                <CardText className="label">
+                  {kukka.kukka2.name}
+                </CardText>
+                <CardText className="label"
+                >{kukka.kukka2.toimi}
+                </CardText>
+                <CardText className="label">
+                  {kukka.kukka2.kerays}
+                </CardText>
+              </CardText>
+              : undefined}
+
+            {kukka.kukka3.toimi !== 0 ?
+              <CardText>
+                <CardText className="label">
+                  {kukka.kukka3.name}
+                </CardText>
+                <CardText className="label"
+                >{kukka.kukka3.toimi}
+                </CardText>
+                <CardText className="label">
+                  {kukka.kukka3.kerays}
+                </CardText>
+              </CardText>
+              : undefined}
+
+            {kukka.kukka4.toimi !== 0 ?
+              <CardText>
+                <CardText className="label">
+                  {kukka.kukka4.name}
+                </CardText>
+                <CardText className="label"
+                >{kukka.kukka4.toimi}
+                </CardText>
+                <CardText className="label">
+                  {kukka.kukka4.kerays}
+                </CardText>
+              </CardText>
+              : undefined}
+
+            {kukka.kukka5.toimi !== 0 ?
+              <CardText>
+                <CardText className="label">
+                  {kukka.kukka5.name}
+                </CardText>
+                <CardText className="label"
+                >{kukka.kukka5.toimi}
+                </CardText>
+                <CardText className="label">
+                  {kukka.kukka5.kerays}
+                </CardText>
+              </CardText>
+              : undefined}
 
             <Button className="buttonC" color="warning" onClick={() => this.props.removePerson(_id)}>Poista</Button> <Button color="primary" onClick={(e) => this.setState({ isOpen: true })}>Muokkaa</Button>
 
-            <Dialog isOpen={this.state.isOpen} onClose={(e) => this.setState({ isOpen: false })} onUpdate={(e) => alert(JSON.stringify({ kukat: { orvokki: { orvokkinimi: this.state.kukka1name, orvtilattu: this.state.kukka1toimi, orvtoimitetaan: 1002, orvlisatieto: "kg", orvjaljella: 1020, orvkerays: "Ryögnä" }, pelargonia: { orvokkinimi: "PgEl", orvtilattu: 120, orvtoimitetaan: 120, orvlisatieto: "kf", orvjaljella: 10, orvkerays: "Tuusjgärvi" } }, name: "Prisma Kguopio" }))}>
+            <Dialog isOpen={this.state.isOpen} onClose={(e) => this.setState({ isOpen: false })}>
               <Card>
                 <CardBlock>
+
                   <CardTitle>{kauppa}</CardTitle>
+
                   <CardText>{_id}</CardText>
-                  <CardText> <CardText className="label">Tuote</CardText><CardText className="label">Kerätään</CardText><CardText className="label">Keräyspiste</CardText></CardText>
-                  {kukka.kukka1.toimi !== 0 ? <CardText><Input type="text" name="kukka1name" onChange={this.handleChange} className="inputlabel" placeholder={kukka.kukka1.name}></Input><Input type="number" name="kukka1toimi" value={this.state.kukanmaara} onChange={this.handleChange} className="inputlabel" placeholder={kukka.kukka1.toimi}></Input><Input className="inputlabel" placeholder={kukka.kukka1.kerays}></Input></CardText> : undefined}
-                  <Button onClick={() => this.props.updatePerson(_id)}>Päivitä</Button>
+
+                  <CardText>
+
+                    <CardText className="label">
+                      Tuote
+                      </CardText>
+
+                    <CardText className="label">
+                      Kerätään
+                        </CardText>
+
+                    <CardText className="label">
+                      Keräyspiste
+                          </CardText>
+
+                  </CardText>
+
+                  {kukka.kukka1.toimi !== 0 ?
+                    <CardText>
+
+                      <Input type="text"
+                        name="nimi1"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka1.name}>
+                      </Input>
+
+                      <Input type="number"
+                        name="maara1"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka1.toimi}>
+                      </Input>
+
+                      <Input className="inputlabel"
+                        name="kerays1"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka1.kerays}>
+                      </Input>
+
+                    </CardText>
+
+                    : undefined}
+
+                  {kukka.kukka2.toimi !== 0 ?
+                    <CardText>
+
+                      <Input type="text"
+                        name="nimi2"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka2.name}>
+                      </Input>
+
+                      <Input type="number"
+                        name="maara2"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka2.toimi}>
+                      </Input>
+
+                      <Input className="inputlabel"
+                        name="kerays2"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka2.kerays}>
+                      </Input>
+
+                    </CardText>
+
+                    : undefined}
+
+                  {kukka.kukka3.toimi !== 0 ?
+                    <CardText>
+
+                      <Input type="text"
+                        name="nimi3"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka3.name}>
+                      </Input>
+
+                      <Input type="number"
+                        name="maara3"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka3.toimi}>
+                      </Input>
+
+                      <Input className="inputlabel"
+                        name="kerays3"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka3.kerays}>
+                      </Input>
+
+                    </CardText>
+
+                    : undefined}
+
+                  {kukka.kukka4.toimi !== 0 ?
+                    <CardText>
+
+                      <Input type="text"
+                        name="nimi4"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka4.name}>
+                      </Input>
+
+                      <Input type="number"
+                        name="maara4"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka4.toimi}>
+                      </Input>
+
+                      <Input className="inputlabel"
+                        name="kerays4"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka4.kerays}>
+                      </Input>
+
+                    </CardText>
+
+                    : undefined}
+
+                  {kukka.kukka5.toimi !== 0 ?
+                    <CardText>
+
+                      <Input type="text"
+                        name="nimi5"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka5.name}>
+                      </Input>
+
+                      <Input type="number"
+                        name="maara5"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka5.toimi}>
+                      </Input>
+
+                      <Input className="inputlabel"
+                        name="kerays5"
+                        onChange={this.handleChange}
+                        className="inputlabel"
+                        placeholder={kukka.kukka5.kerays}>
+                      </Input>
+
+                    </CardText>
+
+                    : undefined}
+
+                  <Button onClick={() => this.patchData(_id)}>Päivitä</Button>
                 </CardBlock>
               </Card>
             </Dialog>
