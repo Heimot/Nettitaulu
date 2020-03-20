ec5842a4a119f00f78afd863a0af701925a6f658
Heimot <heimonenjoonas@gmail.com>
2020-03-20 13:55:30 +0200

    Added excel file to array for stores

--- src/components/Etusivu/frontPage.js
+++ src/components/Etusivu/frontPage.js
@@ -79,6 +79,44 @@ class frontPage extends Component {
         FR.readAsArrayBuffer(file);
     };
 
+    handleFile2 = (e) => {
+        console.log(Object.keys(XLSX.utils))
+        var file = e.target.files[0];
+        // input canceled, return
+        if (!file) return;
+
+        var FR = new FileReader();
+        FR.onload = function (e) {
+            var data = new Uint8Array(e.target.result);
+            var workbook = XLSX.read(data, { type: "array" });
+            var firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+
+            // header: 1 instructs xlsx to create an 'array of arrays'
+            var result = XLSX.utils.sheet_to_csv(firstSheet, { header: 1 });
+
+            console.log(result)
+
+            fetch('http://localhost:3002/items/put/id/5e748700bee89f3d5c27ae55', {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Bearer ' + sessionStorage.getItem('userData')
+                },
+                body: JSON.stringify({
+                  kaupat: result.split("\n")
+                }),
+              })
+                .then(response => response.json())
+                .then(json => {
+                  console.log(json);
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+
+        };
+        FR.readAsArrayBuffer(file);
+    };
 
     render() {
 
@@ -96,7 +134,7 @@ class frontPage extends Component {
                         <Button className="redirect3" onClick={() => this.setState({ redirect: true })}>?????</Button>
                         <Button className="redirect4" onClick={() => this.setState({ redirect: true })}>????</Button>
                         <Input type="file" name="file" id="exampleFile" accept=".xls,.xlsx,.ods" onChange={(e) => this.handleFile(e)}></Input>
-                        <pre id="result"></pre>
+                        <Input type="file" name="file2" id="exampleFile2" accept=".xls,.xlsx,.ods" onChange={(e) => this.handleFile2(e)}></Input>
                     </div>
                     <div className="pictureDot"></div>
                 </div>
