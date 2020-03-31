67d25570c2d8ea791f0ae11e4c1ab85036955ce4
Heimot <heimonenjoonas@gmail.com>
2020-03-31 14:06:40 +0300

    Added the printing window

---
+++ src/components/printWindow/printData.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Table, Thead, Tbody, Tr, Td, Th } from 'react-super-responsive-table';
+import { Button } from 'reactstrap';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+
+//CSS
+import '../../Styles/print.css'
+
+class Printer extends Component {
+    printOrder = () => {
+        const printableElements = document.getElementById('printDiv').innerHTML;
+        const orderHtml = '<html><head><title></title></head><body>' + printableElements + '</body></html>'
+        const oldPage = document.body.innerHTML;
+        document.body.innerHTML = orderHtml;
+        window.print();
+        document.body.innerHTML = oldPage
+        window.location.reload();
+    }
+
+    render() {
+        let { newData } = this.props;
+
+        return (
+            <div>
+                <div id="printDiv" className="printDiv">
+                    <Table id="printTables" className="printTable">
+                        <Thead>
+                            <Tr>
+                                <Th>Tuote</Th>
+                                <Th>Kerätään</Th>
+                                <Th>Lisätietoa</Th>
+                                <Th>Kauppa</Th>
+                            </Tr>
+                        </Thead>
+                        {newData.map(data => {
+                            return (
+                                <Tbody>
+                                    <Tr>
+                                        <Td>{data.tuote}</Td>
+                                        <Td>{data.maara}</Td>
+                                        <Td>{data.lisatieto}</Td>
+                                        <Td>{data.kauppa}</Td>
+                                    </Tr>
+                                </Tbody>
+                            )
+                        })}
+                    </Table>
+                    <Button onClick={() => this.printOrder()}>Tulosta</Button>
+                </div>
+            </div >
+        )
+    }
+}
+
+export default Printer;
