ae6c8ca3119f5bb3b21fed96b13985d20a682782
Heimot <heimonenjoonas@gmail.com>
2020-04-08 16:35:24 +0300

    New path for rullakot.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LogIn from './LogIn';
 import LoginNav from './LoginNav';
 import frontPage from './components/Etusivu/frontPage';
 import ErrorBoundary from './components/errorCatcher/ErrorBoundary';
+import Rullakot from './components/rullakot/rullakot';
 
 class App extends Component {
   render() {
@@ -16,8 +17,9 @@ class App extends Component {
             <Route path="/" exact component={LoginNav} />
             <Route path="/" exact component={LogIn} />
             <Route path="/main" exact component={frontPage} />
-            <Route path="/main/tables" exact component={TopNav} />
-            <Route path="/main/tables" component={MainArea} />
+            <Route path="/main/tables" component={TopNav} />
+            <Route path="/main/tables" exact component={MainArea} />
+            <Route path="/main/tables/rullakot" component={Rullakot} />
           </div>
         </Router>
       </ErrorBoundary>
